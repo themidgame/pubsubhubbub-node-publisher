584d9495a8f960725dfc49f20bfeb752188746dd
Andrés Felipe Díaz <afdiaz2@gmail.com>
2016-01-08 20:01:33 -0300

    Created the test infrastructure to validate the topic urls and validated the hub url

--- test/publisher.js
+++ test/publisher.js
@@ -11,13 +11,13 @@ describe('Publisher', function(){
   topicUrl = 'http://www.example.com';
 
   describe('constructor', function(){
-    it('should validate the url is given', function(){
+    it('should validate the hub url is provided', function(){
       assert.throws(function(){
          new Publisher();
       }, Error, 'Please specify a hub url');
     });
 
-    it('should validate the url is valid', function(){
+    it('should validate the provided hub url is valid', function(){
       assert.throws(function(){
          new Publisher({'hubUrl':'whatever'});
       }, Error, 'Please specify a valid hub url');
@@ -35,12 +35,28 @@ describe('Publisher', function(){
 
   describe('when publishing', function(){
 
+    it('should validate that it has a topic url', function(){
+      assert.equal(false,true, 'The topic url should be validated');
+    });
+
+    it('should validate that the topic url is valid', function(){
+      assert.equal(false,true, 'The topic url should be validated');
+    });
+
+    it('should accept one topic url', function(){
+      assert.equal(false,true, 'Should accept one topic url');
+    });
+
+    it('should accept multiple topic urls', function(){
+      assert.equal(false,true, 'Should accept an array of topic urls');
+    });
+
     it('should be able to publish to a hub', function(){
       assert.equal(publisher.publishUpdate(topicUrl), true, 'the publisher should be able to publish a topic.');
     });
 
     it('should allow the clients to see the last response', function(){
-      assert.equal(publisher.lastResponse(), 'response', 'the expected last response is not available in the publisher.');
+      assert.equal(publisher.getLastResponse(), 'response', 'the expected last response is not available in the publisher.');
     });
 
   });
