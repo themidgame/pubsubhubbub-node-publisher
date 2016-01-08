f56d60722375c7f1e63e158f7c9f410472e874f1
Andrés Felipe Díaz <afdiaz2@gmail.com>
2016-01-08 19:49:41 -0300

    Validated that the hub url is provided and valid

--- test/publisher.js
+++ test/publisher.js
@@ -7,15 +7,42 @@ var mocha = require('mocha'),
 
 describe('Publisher', function(){
   var hubUrl = 'http://pubsubhubbub.appspot.com/',
-  publisher = new Publisher(hubUrl),
+  publisher = new Publisher({'hubUrl': hubUrl}),
   topicUrl = 'http://www.example.com';
 
-  it('should be able to publish to a hub', function(){
-    assert.equal(publisher.publishUpdate(topicUrl), true, 'the publisher should be able to publish a topic.');
+  describe('constructor', function(){
+    it('should validate the url is given', function(){
+      assert.throws(function(){
+         new Publisher();
+      }, Error, 'Please specify a hub url');
+    });
+
+    it('should validate the url is valid', function(){
+      assert.throws(function(){
+         new Publisher({'hubUrl':'whatever'});
+      }, Error, 'Please specify a valid hub url');
+      assert.throws(function(){
+         new Publisher({'hubUrl':'hxttp://whatever.com'});
+      }, Error, 'Please specify a valid hub url');
+      assert.doesNotThrow(function(){
+         new Publisher({'hubUrl':'http://whatever.com'});
+      });
+      assert.doesNotThrow(function(){
+         new Publisher({'hubUrl':'https://whatever.com'});
+      });
+    });
   });
 
-  it('should allow the clients to see the last response', function(){
-    assert.equal(publisher.lastResponse(), 'response', 'the expected last response is not available in the publisher.');
+  describe('when publishing', function(){
+
+    it('should be able to publish to a hub', function(){
+      assert.equal(publisher.publishUpdate(topicUrl), true, 'the publisher should be able to publish a topic.');
+    });
+
+    it('should allow the clients to see the last response', function(){
+      assert.equal(publisher.lastResponse(), 'response', 'the expected last response is not available in the publisher.');
+    });
+
   });
 
 });
